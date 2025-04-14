1d33029a23fac3a7ec7fa08a437e51aeceee8f02
Tiina Siremaa <tiinatuuliak@gmail.com>
2025-04-14 13:08:42 +0300

    luotu routeja

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+
+// routes
+import authRoutes from "./routes/auth.route.js";
+
+dotenv.config();
+//luetaan .env tiedoston sisältö
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use("/api/auth", authRoutes)
+
+
+app.listen(PORT, () => {
+    console.log("Serveri käynnissä OSOITTEESSA http://localhost:" + PORT);
+});

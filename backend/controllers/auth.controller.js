682fa63c47be7343acbce1c6696d824ab80a945c
Tiina Siremaa <tiinatuuliak@gmail.com>
2025-04-15 11:36:51 +0300

    cookieiden tyhjennys (clear) kun kirjaudutaan ulos, valmis

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -78,5 +78,19 @@ export const login = async (req, res) => {
 }
 
 export const logout = async (req, res) => {
-    res.send("Log out route called");
+    try {
+        const refreshToken = req.cookies.refreshToken;
+        if(refreshToken){
+            const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+            await redis.del(`refresh_token:${decoded.userId}`);
+           
+        }
+
+        res.clearCookie("accessToken");
+        res.clearCookie("refreshToken");
+        
+        res.json({message: "Logout onnistui"}).sendStatus(200);
+    } catch (error) {
+        res.status(500).json({ message: "Serveri ei vastaa", error: error.message });
+    }
 };

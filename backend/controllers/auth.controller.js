07ab14683987bb07d468aed5ce1bab0724033f49
Tiina Siremaa <tiinatuuliak@gmail.com>
2025-04-22 17:08:00 +0300

    sisäänpääsytokenin päivitys metodi valmis

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -67,7 +67,7 @@ export const signup = async (req, res) => {
     }, message: "Käyttäjä luotu onnistuneesti!"
 });
     } catch (error) {
-        console.log("Virhe yhdistettäessä MongoDB tietokantaan", error.message);
+        console.log("Virhe sign up controllerissa", error.message);
         res.status(500).json({ message: error.message });
     }
 
@@ -77,20 +77,24 @@ export const signup = async (req, res) => {
 export const login = async (req, res) => {
     try {
         const { email, password } = req.body;
-        const user = await User.findOne({ email })
+        const user = await User.findOne({ email });
+        
 
         if(user && (await user.comparePassword(password))) {
-            const {accessToken, refreshToken} = generateTokens(user._id)
+            const {accessToken, refreshToken} = generateTokens(user._id);
+            //console.log("user is logged in"); testattu loginin toimivuutta 
 
-            await storeRefreshToken(user._id, refreshToken)
-            setCookies(res, accessToken, refreshToken)
+            await storeRefreshToken(user._id, refreshToken);
+            setCookies(res, accessToken, refreshToken);
 
             res.json({
                 _id: user._id,
                 name: user.name,
                 email: user.email,
-                role: user.role
+                role: user.role,
             })
+        } else {
+            res.status(401).json({ message: "Virheellinen email tai salasana"});
         }
     } catch (error) {
         console.log("Error sisäänkirjautumiscontrollerissa", error.message);
@@ -113,6 +117,55 @@ export const logout = async (req, res) => {
         
         res.json({message: "Logout onnistui"}).sendStatus(200);
     } catch (error) {
+        console.log("Virhe logoutissa", error.message);
         res.status(500).json({ message: "Serveri ei vastaa", error: error.message });
     }
 };
+
+//päivittää sisäänpääsytokenin joka vanhenee 15 minuutissa
+
+export const refreshToken = async (req, res) => {
+    try {
+        const refreshToken = req.cookies.refreshToken;
+        if(!refreshToken) {
+
+            return res.status(401).json({message: "Ei päivitettyä refresh tokenia"});
+        }
+        const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+        const storedToken = await redis.get(`refresh_token:${decoded.userId}`);
+
+        if(storedToken !== refreshToken) {
+            return res.status(401).json({message: "Ei vastaavaa refresh tokenia"});
+        }
+
+        const accessToken = jwt.sign({userId: decoded.userId}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: "15m"});
+        res.cookie("accessToken", accessToken, {
+            httpOnly: true, //estä xss hyökkäykset, cookieen ei pääse käsiksi esim javascriptin kautta
+            secure:  process.env.NODE_ENV === "production",
+            sameSite:"strict", //estää CSRF hyökkäykset
+            maxAge: 15 * 60 * 1000, //15 minuuttia
+        });
+
+        res.status(200).json({message: "Refresh token päivitetty"});
+
+
+        } catch (error) {
+        console.log("Virhe refresh token controllerissa", error.message);
+        res.status(500).json({ message: "Serveri ei vastaa", error: error.message });
+        
+    }
+}
+
+//tehdään myöhemmin.....
+//export const getProfile = async (req, res) => {
+
+
+
+
+//testattu että accesstoken tulee varmasti uutena kun se päivitetään (refresh)
+
+//accessToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2N2ZkMmI2NmQ3Yjk5MDViZmZmNzQ3YTkiLCJpYXQiOjE3NDUzMjk3OTQsImV4cCI6MTc0NTMzMDY5NH0.C2Yz2yGfCeuJawAL0bmg4V1hqhp55v0zCAB_wL6EWm0; 
+// Path=/; HttpOnly; Expires=Tue, 22 Apr 2025 14:04:54 GMT;
+
+//accessToken=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI2N2ZkMmI2NmQ3Yjk5MDViZmZmNzQ3YTkiLCJpYXQiOjE3NDUzMjk5NDQsImV4cCI6MTc0NTMzMDg0NH0.nttOwDY3Alawl2r_ZPdP7JDFIil7uJspy3tzdLTOOqU; 
+// Path=/; HttpOnly; Expires=Tue, 22 Apr 2025 14:07:24 GMT;
--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 
 // routes
 import authRoutes from "./routes/auth.route.js";
+import productRoutes from "./routes/product.route.js";
 
 import { connectDB } from "./lib/db.js";
 
@@ -17,7 +18,8 @@ app.use(express.json()); // allows you to parse the body of the request
 
 app.use(cookieParser());
 
-app.use("/api/auth", authRoutes)
+app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
 
 
 app.listen(PORT, () => {

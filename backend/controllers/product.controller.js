1093c642d37cfbff2387f34e6d8cc4a481d4d7d9
Tiina Siremaa <tiinatuuliak@gmail.com>
2025-04-22 18:23:42 +0300

    featuredProducts luotu controlleriin

--- backend/controllers/product.controller.js
+++ backend/controllers/product.controller.js
@@ -1,5 +1,5 @@
 import Product from "../models/product.model.js";
-
+import { redis } from "../lib/redis.js";
 
 
 export const getAllProducts = async (req, res) => {
@@ -11,3 +11,31 @@ export const getAllProducts = async (req, res) => {
         res.status(500).json({ message: "Serveri ei vastaa", error: error.message });
     }
 };    
+
+export const getFeaturedProducts = async (req, res) => {
+    try {
+        let featuredProducts = await redis.get("featured_products"); //etsi kaikki tuotteet
+        if(!featuredProducts) {
+            return res.json(JSON.parse(featuredProducts));
+        }
+
+        //jos festured tuotteita ei ole rediksessä, etsi tietokannasta
+
+        featuredProducts = await Product.find({Isfeatured: true}).lean();  //etsi kaikki tuotteet MONGO dbstä 
+        // //.lean palauttaa javascript objekteja mongo db dokumentin sijasta
+        //parantaa suorituskykyä
+
+        if(!featuredProducts) {
+            return res.status(404).json({message: "Ei featured- tuotteita"});
+        }
+
+        //tallenntaa redikseen jos löytyy mongosta mutta ei vielä sieltä
+
+        await redis.set("featured_products", JSON.stringify(featuredProducts));
+        res.json(featuredProducts);
+
+    } catch (error) {
+        console.log("Virhe tuotecontrollerissa", error.message);
+        res.status(500).json({ message: "Serveri ei vastaa", error: error.message });
+    }
+};

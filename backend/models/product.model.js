79b53b649c16f109624935f8e8f760c9fd9c8bcb
Tiina Siremaa <tiinatuuliak@gmail.com>
2025-04-22 17:23:58 +0300

    luotu product model ja product route

---
+++ backend/models/product.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Nimi on pakollinen tieto"],
+    },
+    description: {
+        type: String,
+        required: [true, "Kuvaus on pakollinen tieto"],
+    },
+    price: {
+        type: Number,
+        min: 0,
+        required: [true, "Hinta on pakollinen tieto"]
+    },
+    image: {
+        type: String,
+        required: [true, "Kuvan URL on pakollinen tieto"]
+    },
+    category: {
+        type: String,
+        required: [true, "Kategoria on pakollinen tieto"]
+    }, 
+    isFeatured: {
+        type: Boolean,
+        default: false
+    }
+},{timestamps: true});
+
+const Product = mongoose.model("Product", productSchema);
+
+export default Product

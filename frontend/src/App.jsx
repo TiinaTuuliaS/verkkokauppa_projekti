ceaa61e92d6432126bbbb78cdf1974f1e7abba67
Tiina Siremaa <tiinatuuliak@gmail.com>
2025-05-27 16:06:08 +0300

    luotu sivuja fronttiin, ongelmia tailwindin kanssa, fiksattu

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,25 @@
+import { Route, Routes } from "react-router-dom"
+import HomePage from "./pages/HomePage.jsx"
 
 
 function App() {
   return (
-    <div className= 'text-red-500 text-5xl'>
-     Hello World!
+    <div>
+<Routes>
+
+<Route path="/" element={<HomePage/>} />
+<Route path="/signup" element={<SignUpPage/>} />
+<Route path="/login" element={<LoginPage/>} />
+
+
+
+
+</Routes>
+
+
+
+
+
     </div>
   )
 }

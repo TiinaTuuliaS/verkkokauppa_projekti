b6e95332101a9891e4a19c777ac78c91a9a03560
Tiina Siremaa <tiinatuuliak@gmail.com>
2025-08-30 22:16:03 +0300

    lisätty build package jsoniin ja server js tiedostoon

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 // Tuodaan eri reittien moduulit
 import authRoutes from "./routes/auth.route.js";
@@ -18,6 +19,8 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+const __dirname = path.resolve();
+
 app.use(express.json({ limit: "10mb"})); // 10mb tiedostojen koko rajoitus http pyynnöissä esim post ja put
 
 app.use(cookieParser());
@@ -31,6 +34,14 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "frontend/dist")));
+    
+    app.get("*", (req, res) =>
+        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
+    );
+}
 
 //palvelin käynnistetään ja tietokantayhteys muodostetaan
 

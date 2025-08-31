43303d25fc22626df3cfb93047d9e3ea45180aa2
Tiina Siremaa <tiinatuuliak@gmail.com>
2025-08-31 12:56:12 +0300

    deployment ongelmat jatkuvat

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser";
 import path from "path";
 import { fileURLToPath } from "url";
 
-// Tuodaan eri reittien moduulit
+// Tuodaan reitit
 import authRoutes from "./routes/auth.route.js";
 import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
@@ -12,21 +12,21 @@ import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.route.js";
 import analyticsRoutes from "./routes/analytics.route.js";
 
-import { connectDB } from "./lib/db.js"; // tietokantayhteyden muodostaminen
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-// __dirname polyfill (koska käytetään ES Module -syntaksia)
+// __dirname polyfill ES moduuleissa
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 
-// Reittien liittäminen sovellukseen
+// Reitit
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
@@ -34,17 +34,17 @@ app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
-app.use(express.static(path.join(__dirname, "frontend/dist")));
+// Staattiset tiedostot vain productionissa
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-app.get("/*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-});
+  app.get("/*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+  });
+}
 
-
-// Palvelin käynnistetään ja tietokantayhteys muodostetaan
+// Palvelin käyntiin ja tietokantayhteys
 app.listen(PORT, () => {
   console.log(`Serveri käynnissä OSOITTEESSA http://localhost:${PORT}`);
   connectDB();
 });
-
-

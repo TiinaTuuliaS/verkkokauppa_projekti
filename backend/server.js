b71a7eae92708ae3bc79a3eb432d12ceb9e3c177
Tiina Siremaa <tiinatuuliak@gmail.com>
2025-08-31 12:01:45 +0300

    muokattu riviä 39, debuggaus jatkuu

--- backend/server.js
+++ backend/server.js
@@ -34,15 +34,12 @@ app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
-// Production build (Render)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.use(express.static(path.join(__dirname, "frontend/dist")));
 
-  // React catch-all route
-  app.get("/*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
-}
+app.get("/*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
+
 
 // Palvelin käynnistetään ja tietokantayhteys muodostetaan
 app.listen(PORT, () => {

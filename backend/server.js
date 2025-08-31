fd237c1d46036d3d9ec951b608e4bb2033a49364
Tiina Siremaa <tiinatuuliak@gmail.com>
2025-08-31 11:27:13 +0300

    ongelmia deploymentissa muokattu render koodia

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import path from "path";
+import { fileURLToPath } from "url";
 
 // Tuodaan eri reittien moduulit
 import authRoutes from "./routes/auth.route.js";
@@ -11,23 +12,21 @@ import couponRoutes from "./routes/coupon.route.js";
 import paymentRoutes from "./routes/payment.route.js";
 import analyticsRoutes from "./routes/analytics.route.js";
 
-import { connectDB } from "./lib/db.js"; //tietokantayhteyden muodostaminen
+import { connectDB } from "./lib/db.js"; // tietokantayhteyden muodostaminen
 
 dotenv.config();
-//luetaan .env tiedoston sisältö ja asennetaan ympäristömuuttujat
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-const __dirname = path.resolve();
+// __dirname polyfill (koska käytetään ES Module -syntaksia)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.use(express.json({ limit: "10mb"})); // 10mb tiedostojen koko rajoitus http pyynnöissä esim post ja put
-
-app.use(cookieParser());
-// Käytetään evästeiden käsittelyä, esim. käyttäjän autentikointiin
+app.use(express.json({ limit: "10mb" })); // 10mb tiedostojen koko rajoitus http-pyynnöissä esim. post ja put
+app.use(cookieParser()); // Käytetään evästeiden käsittelyä, esim. autentikointiin
 
 // Reittien liittäminen sovellukseen
-
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
@@ -35,20 +34,19 @@ app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
 
+// Production build (Render)
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "frontend/dist")));
-    
-    app.get("*", (req, res) =>
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
-    );
+  // Palvellaan frontendin build kansiosta
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  // React catch-all route
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+  });
 }
 
-//palvelin käynnistetään ja tietokantayhteys muodostetaan
-
+// Palvelin käynnistetään ja tietokantayhteys muodostetaan
 app.listen(PORT, () => {
-    console.log("Serveri käynnissä OSOITTEESSA http://localhost:" + PORT);
-
-    connectDB();
+  console.log(`Serveri käynnissä OSOITTEESSA http://localhost:${PORT}`);
+  connectDB();
 });
-
-// 0zDQ4zKOIf6WtJDb
